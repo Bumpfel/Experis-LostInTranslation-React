428136f77832a28b4e0cb851ac9f090529513286
Bumpfel <eric.enoksson@gmail.com>
2020-11-24 19:24:14 +0100

    refactor(translate-page): using ref & arrow functions

--- src/pages/TranslatePage/index.js
+++ src/pages/TranslatePage/index.js
@@ -4,19 +4,25 @@ import './translate.css'
 
 export default class TranslatePage extends React.Component {
 
+  searchBar = React.createRef()
+
   state = {
     text: '',
     translation: [],
   }
 
-  componentDidMount() {
-    document.querySelector('#search').focus()
+  componentDidMount = () => {
+    // this.mock()
+  }
+
+  mock = () => { // debug
+    this.searchBar.current.value = 'Hej, hur mår du?!'
+    this.translate({ preventDefault: () => {} })
   }
 
   translate = e => {
     e.preventDefault()
-    const text = e.target.querySelector('#search').value
-    // const text = 'Hej, hur mår du?!' // mockText
+    const text = this.searchBar.current.value
     const translation = []
 
     for (let i = 0; i < text.length; i++) {
@@ -24,35 +30,33 @@ export default class TranslatePage extends React.Component {
         <Sign key={i} char={text[i]} />
       )
     }
-
+    this.searchBar.current.value = ''
     this.setState({ text, translation })
   }
 
-  render() {
-    return (
-      <React.Fragment>
-        <h2>Translate to sign language</h2>
+  render = () => (
+    <React.Fragment>
+      <h2>Translate to sign language</h2>
 
-        <form onSubmit={this.translate} className="row">
-          <div className="col-8">
-            <input className="form-control" type="search" placeholder="Enter a term to translate" id="search" />
-          </div>
-          <div className="col">
-            <button className="btn btn-outline-secondary">Translate</button>
-          </div>
-        </form>
+      <form onSubmit={this.translate} className="row">
+        <div className="col-8">
+          <input className="form-control" type="search" placeholder="Enter a term to translate" ref={this.searchBar} autoFocus />
+        </div>
+        <div className="col">
+          <button className="btn btn-outline-secondary">Translate</button>
+        </div>
+      </form>
 
-        {this.state.translation.length > 0 &&
-          <div className="mt-5">
-            <h4>Translated</h4>
-            {this.state.text}
-            <div className="border rounded p-2 translationBox">
-              {this.state.translation}
-            </div>
+      {this.state.translation.length > 0 &&
+        <div className="mt-5">
+          <h4>Translated</h4>
+          
+          {this.state.text}
+          <div className="border rounded p-2 translationBox">
+            {this.state.translation}
           </div>
-        }
-
-      </React.Fragment>
-    )
-  }
+        </div>
+      }
+    </React.Fragment>
+  )
 }
