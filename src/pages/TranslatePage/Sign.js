b5627ba573412921276abdb6bcf8057cffabb8e3
Bumpfel <eric.enoksson@gmail.com>
2020-11-24 17:23:11 +0100

    feat(translate): translation working

---
+++ src/pages/TranslatePage/Sign.js
@@ -0,0 +1,14 @@
+const isValidChar = ch => ch.match(RegExp(/[a-z]/i))
+const isWhiteSpace = ch => ch.match(RegExp(/\s/))
+
+const Sign = (props) => {
+  if(isValidChar(props.char)) {
+    return <img className="sign" src={`/sprites/hand-signs/${props.char}.png`} alt={props.char} /> // valid hand sign (a-z)
+
+  } else if(isWhiteSpace(props.char)) {
+    return <span className="spacer"></span> // space
+  }
+
+  return <span>{props.char}</span> // other char
+}
+export default Sign
--- src/pages/TranslatePage/index.js
+++ src/pages/TranslatePage/index.js
@@ -1,15 +1,57 @@
 import React from 'react'
+import Sign from './Sign'
+import './translate.css'
 
 export default class TranslatePage extends React.Component {
 
-  state = {}
+  state = {
+    text: '',
+    translation: [],
+  }
 
-  componentDidMount() { }
+  componentDidMount() {
+    document.querySelector('#search').focus()
+  }
+
+  translate = e => {
+    e.preventDefault()
+    const text = e.target.querySelector('#search').value
+    // const text = 'Hej, hur mår du?!' // mockText
+    const translation = []
+
+    for (let i = 0; i < text.length; i++) {
+      translation.push(
+        <Sign key={i} char={text[i]} />
+      )
+    }
+
+    this.setState({ text, translation })
+  }
 
   render() {
     return (
       <React.Fragment>
-        Translate page
+        <h2>Translate to sign language</h2>
+
+        <form onSubmit={this.translate} className="row">
+          <div className="col-8">
+            <input className="form-control" type="search" placeholder="Enter a term to translate" id="search" />
+          </div>
+          <div className="col">
+            <button className="btn btn-outline-secondary">Translate</button>
+          </div>
+        </form>
+
+        {this.state.translation.length > 0 &&
+          <div className="mt-5">
+            <h4>Translated</h4>
+            {this.state.text}
+            <div className="border rounded p-2 translationBox">
+              {this.state.translation}
+            </div>
+          </div>
+        }
+
       </React.Fragment>
     )
   }
