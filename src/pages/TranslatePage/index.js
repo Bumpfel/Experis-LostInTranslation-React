4cb0ef1ec41d64933feb57cd61ff79b34876295f
PerChrister <perchrister96@gmail.com>
2020-11-25 17:19:00 +0100

    fix: Modified translate page and storage.js

--- src/pages/TranslatePage/index.js
+++ src/pages/TranslatePage/index.js
@@ -1,16 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { clearTranslations, loadTranslations, storeTranslation } from '../../utils/storage';
 
-export default class TranslatePage extends React.Component {
+const TranslatePage = (props) => {
 
-  state = {}
+  const [translation, setTranslation] = useState(['']);
 
-  componentDidMount() { }
+  const onUserSubmit = () => {
 
-  render() {
-    return (
-      <React.Fragment>
-        Translate page
-      </React.Fragment>
-    )
+    storeTranslation(translation);
+    
   }
+
+  const getTranslation = () => {
+    console.log(loadTranslations);
+  }
+
+  let onTranslationChanged = ev => setTranslation(ev.target.value.trim());
+
+  return (
+    <div>
+      <form onSubmit={onUserSubmit}>
+        <div className="form-group">
+          <label>Translate: </label>
+          <input className="form-control" type="text" autoComplete="off" placeholder="Enter word to translate" onChange={onTranslationChanged} />
+        </div>
+        <div>
+          <button className="btn btn-outline-secondary">Translate</button>
+        </div>
+
+      </form>
+        <div>
+          <button className="btn btn-outline-secondary" onClick={getTranslation}>Get translation</button>
+        </div>
+        <div>
+          <button className="btn btn-outline-secondary"onClick={clearTranslations}>Remove translation</button>
+        </div>
+    </div>
+  )
+
 }
+
+export default TranslatePage;
--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,12 +1,12 @@
-export const storeTranslation = () => {
-
+export const storeTranslation = (translation) => {
+    localStorage.setItem('translation', JSON.stringify(translation));
 }
 
 export const loadTranslations = () => {
-
+    localStorage.getItem('translation');
 }
 
 export const clearTranslations = () => {
-
+    localStorage.removeItem('translation');
 }
 
