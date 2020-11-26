539a60791d9d67027eb71e9184dcb068f07becbf
PerChrister <perchrister96@gmail.com>
2020-11-26 16:01:35 +0100

    fix: Fixed auth conditions and stylings

--- src/components/TopMenu.js
+++ src/components/TopMenu.js
@@ -1,37 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import * as Auth from '../utils/auth'
 import { getUser } from '../utils/storage'
 
-const isLoggedIn = () => Auth.isLoggedIn()
+const getLoginStatus = () => Auth.isLoggedIn()
 const showUser = getUser();
 
 const TopMenu = props => {
 
+  const [isLoggedIn, setIsLoggedIn] = useState(getLoginStatus);
+
   const onLogoutClick = () => {
     Auth.logout();
+    setIsLoggedIn(false);
   }
+
 
   const userName = {
     fontColor: 'black',
     fontFamily: 'Segoe UI',
     fontSize: '18px',
     fontWeight: 'bold',
-    marginTop: '6px'
+    marginTop: '7px'
   };
 
   return (
     <nav className="navbar navbar-expand-sm navbar-light bg-light">
       <Link className="navbar-brand" to="/">Menu</Link>
       <div className="navbar-nav">
-        {isLoggedIn && <span style={userName}>{showUser}</span>}
         <NavLink className="nav-item nav-link" to="/translate">Translate</NavLink>
         <NavLink className="nav-item nav-link" to="/profile">Profile</NavLink>
-        <NavLink className="nav-item nav-link" to="/login">Login</NavLink>
+        <NavLink className="nav-item nav-link" to="/login">
+        { !isLoggedIn && <span>Login</span> }
+        </NavLink>
         <NavLink className="nav-item nav-link" to="/login" onClick={onLogoutClick}>
           { isLoggedIn && <span>Logout</span> }
-          { !isLoggedIn && <span></span> }
           </NavLink>
+        { isLoggedIn && <span style={userName}>{showUser}</span> }
       </div>
     </nav>
   )
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -20,11 +20,15 @@ const LoginPage = (props) => {
 
     const onUsernameChanged = ev => setUsername(ev.target.value.trim());
 
+    const inputStyle = {
+        width: '275px'
+    }
+
     return (
         <form onSubmit={onUserSubmit}>
             <div className="form-group">
                 <label>Username: </label>
-                <input className="form-control" type="text" autoComplete="off" placeholder="Enter username of choice to log in" onChange={onUsernameChanged} />
+                <input className="form-control" type="text" autoComplete="off" placeholder="Enter username of choice to log in" style={inputStyle} onChange={onUsernameChanged} />
             </div>
             <div>
                 <button className="btn btn-outline-secondary">Login</button>
