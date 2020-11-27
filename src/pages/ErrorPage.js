a2ea8109bb4285529a10a315815db61bfc996a82
Bumpfel <eric.enoksson@gmail.com>
2020-11-27 11:53:34 +0100

    fix(errorpage): tiny - link that actually goes back

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
-const ErrorPage = (props) => (
+const ErrorPage = (props) => {
+
+    return (
     <div className="container mt-4">
         <h1>{props.header}</h1>
         <p>{props.message}</p>
 
-        <Link to="/">Go Back</Link>
+        <button className="btn btn-link p-0" onClick={() => window.history.back()}>Go Back</button>
     </div>
-)
+    )
+}
 export default ErrorPage
